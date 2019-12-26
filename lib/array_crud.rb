8ad26a689d1231809f283f448b9bf57b2f2bc0cc
Firuz Afzalov <afzalov98@gmail.com>
2019-12-26 01:50:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,30 +1,42 @@
-def create_an_empty_array
-  
+def 
+  create_an_empty_array 
+  []
 end
 
-def create_an_array
-  
+
+def create_an_array 
+  ['cat','dog','cow','lion']
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    array.push(element)
 end
+add_element_to_end_of_array(["English","Russian"],"Tajik")
+
+
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
+add_element_to_start_of_array(["Persian","Turkish"],"France")
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop()
 end
+remove_element_from_end_of_array(["Pers","Turk"])
+
 
 def remove_element_from_start_of_array(array)
+  array.shift()
   
 end
+remove_element_from_start_of_array("Messi","Ronaldo")
 
 def retrieve_element_from_index(array, index_number)
+  array.index_number[2]
   
 end
+retrieve_element_from_index("Soccer","Basketball")
 
 def retrieve_first_element_from_array(array)
   
