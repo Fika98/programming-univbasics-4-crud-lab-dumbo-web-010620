636e29051d7e2fd96c0012601894667e934aa28e
Firuz Afzalov <afzalov98@gmail.com>
2019-12-26 02:09:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,7 +33,7 @@ end
 remove_element_from_start_of_array(["Messi","Ronaldo"])
 
 def retrieve_element_from_index(array, index_number)
-  array.index|index_number|
+  array.index[index_numbe]
   
 ends
 retrieve_element_from_index(["hello","Basketball"],0)
