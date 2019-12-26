76bd86748595e505311c52af5cf3d698c5b8aa7d
Firuz Afzalov <afzalov98@gmail.com>
2019-12-26 02:12:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -36,7 +36,7 @@ def retrieve_element_from_index(array, index_number)
   array.index(index_number)
   
 ends
-retrieve_element_from_index(["hello","Basketball"],[""])
+retrieve_element_from_index(["hello","Basketball"],["hello"])
 
 def retrieve_first_element_from_array(array)
   
